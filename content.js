ea5beb57af5221e533fc93476385c6c4a38396f8
Catalin ZANFIR <catalin@adoreme.com>
2020-06-25 15:32:09 +0300

    update card size

--- content.js
+++ content.js
@@ -11,6 +11,12 @@ setInterval(function(){
             item.style.display = "none";
         });
 
+        var issues = document.querySelectorAll('.ghx-issue');
+        issues.forEach(function (item) {
+            item.style.padding = "5px 10px 5px 5px";
+            item.style.margin = "5px 10px 5px 5px";
+        });
+
         var issuesContent = document.querySelectorAll('.ghx-issue .ghx-issue-content');
         issuesContent.forEach(function (item) {
             item.style.minHeight = 0;
